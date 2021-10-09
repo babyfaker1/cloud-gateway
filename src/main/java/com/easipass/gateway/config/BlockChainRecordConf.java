8131c421fdfcaefa1b7f6e41a638da3eff8bf21d
chiswang <chiswang@cisco.com>
2021-10-09 13:27:42 +0800

    dev6

--- src/main/java/com/easipass/gateway/config/BlockChainRecordConf.java
+++ src/main/java/com/easipass/gateway/config/BlockChainRecordConf.java
@@ -26,7 +26,7 @@ public class BlockChainRecordConf {
     }
 
     @PostConstruct
-    public void init() {      //dev   //dev2         //dev4  //dev5
+    public void init() {      //dev   //dev2         //dev4  //dev5   //dev6
         ConfigFile config = ConfigService.getConfigFile("BlockChainRecordConf" , ConfigFileFormat.JSON);
         changeConf(config.getContent());
         config.addChangeListener((ConfigFileChangeEvent changeEvent) -> {
